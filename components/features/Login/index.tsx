b6bf7cdba8b6dbd03fe56ea8fe15f0711f0dbcd9
권은서 <kwonenseo@gmail.com>
2022-09-26 03:43:17 +0900

    chore: set stylelint and scss

---
+++ components/features/Login/index.tsx
@@ -0,0 +1,11 @@
+import styles from './login.module.scss'
+
+const Login = () => {
+  return (
+    <div className={styles.loginMain}>
+      <h1>Login Page</h1>
+    </div>
+  )
+}
+
+export default Login
